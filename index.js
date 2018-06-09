a507d2717ee1979cec6185d9046e9b81e5198524
ajay2507 <pajay2507@gmail.com>
2018-06-08 23:37:23 -0700

    added build.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const acorn = require('acorn');
 const fs = require('fs');
+const walk = require('acorn/dist/walk');
 
-module.exports = {};
+function parseLassoBundle() {
+    const content = fs.readFileSync(__dirname + 'lib/build.js', 'utf8');
+    const ast = acorn.parse(content, {
+        sourceType: 'script'
+    });
+}
+
+module.exports = { parseLassoBundle };
