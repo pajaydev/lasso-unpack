5327d40a52da5c6085d22e93d78d4da050be796e
ajay2507 <pajay2507@gmail.com>
2018-07-12 00:08:47 -0700

    support for cli

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,15 @@
 const parseArgs = require('minimist');
 const lassoUnpack = require('../lib/lasso-unpack');
 
-if (!argv) {
-    argv = process.argv.slice(2);
+const argv = parseArgs(process.argv.slice(2));
+
+const input = argv._ || [];
+
+if (input.length > 0) {
+    input.map((fileName) => {
+        lassoUnpack(fileName);
+    });
+} else {
+    console.log("No input provided\nUsage: lasso-unpack < bundle.js >");
+    return process.exit(1);
 }
